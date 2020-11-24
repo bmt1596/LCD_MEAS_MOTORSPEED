9817d0ac0224ab3806059ea925161f8760821203
minht <minht@DESKTOP-ARAA3PJ>
2020-11-24 13:11:18 +0100

    add: new code to display the number on screen

--- main.c
+++ main.c
@@ -16,64 +16,116 @@ int colorarray[] = { 0x00000000,  //  BLACK 0
             };
 
 void init_and_paintbackground(void);
+void displayzahl_and_kreis(void);
+void display_number(void);
+
+
 
 // same values as array for indexed colors
 // Variable zum Auslesen des Systemtaktes
 uint32_t sysClock;
+
+void display_information(void);
+
 
 void init_and_paintbackground(void)
 {
     // Init Port L for Display Control and Port M for Display Data
     init_ports_display();
     // Display initialization
+    displayzahl_and_kreis();
+
+    //number on display
+
+    display_number();
+}
+
+void display_information(void)
+{
     set_background_color(colorarray[0]);
-
-
-    // drawline_H(0, 800, 200, colorarray[0], 2);
-
+    drawline_H(551, 800, 170, colorarray[1], 2);
     drawline_V(0, 480, 550, colorarray[1], 2);
+    // for unter line
+    drawline_H(621, 738, 58, colorarray[1], 1);
 
     // Header
-    print_string1216("                    Fahzeug-Informations-Display                   ", 0, 0, colorarray[1], colorarray[5]);
+    print_string1216(
+            "                    Fahzeug-Informations-Display                   ",
+            0, 0, colorarray[1], colorarray[5]);
+
+    print_string1216("MC Projekt",
+            40, 622, colorarray[1], colorarray[0]);
+
+    print_string1216("Mitglieder:",
+                80, 565, colorarray[1], colorarray[0]);
+    print_string1216("1. Minh Tung Bui",
+                    105, 565, colorarray[1], colorarray[0]);
+    print_string1216("2. Hauke Kosmiter",
+                        125, 565, colorarray[1], colorarray[0]);
+
+
+
+}
+
+void displayzahl_and_kreis(void)
+{
+
+
+    display_information();
+    // gach do va so
+    // void write_line(short x1, short y1, short x2, short y2, int color)
+    drawline(84, 377, 101, 369, colorarray[3]);
+    print_string1216("0", 358, 108, colorarray[6], colorarray[0]);
+
+    drawline_H(53, 72, 270, colorarray[3], 1);
+    print_string1216("30", 265, 78, colorarray[6], colorarray[0]);
+
+    drawline(83, 158, 104, 166, colorarray[3]);
+    print_string1216("60", 165, 112, colorarray[6], colorarray[0]);
+
+    drawline(163, 80, 171, 99, colorarray[3]);
+    print_string1216("90", 106, 166, colorarray[6], colorarray[0]);
+
+    drawline_V(48, 69, 275, colorarray[3], 1);
+    print_string1216("120", 75, 257, colorarray[6], colorarray[0]);
+
+    drawline(390, 80, 382, 99, colorarray[3]);
+    print_string1216("150", 109, 360, colorarray[6], colorarray[0]);
+
+    drawline(465, 162, 448, 170, colorarray[3]);
+    print_string1216("180", 176, 420, colorarray[6], colorarray[0]);
+
+    drawline_H(476, 496, 270, colorarray[3], 1);
+    print_string1216("210", 265, 437, colorarray[6], colorarray[0]);
+
+    drawline(465, 376, 448, 368, colorarray[3]);
+    print_string1216("240", 348, 420, colorarray[6], colorarray[0]);
+
+    print_string1216("Km/h", 135, 253, colorarray[1], colorarray[0]);
 
     // vong tron mau vang
-    drawCircle_px(275, 270, 225, colorarray[6], 2);
+    drawCircle_px(275, 270, 222, colorarray[6], 3);
 
     // unter black line
     drawline_H(0, 550, 390, colorarray[0], 100);
 
     // ausser Kreis
-    drawCircle_px(275, 270, 235, colorarray[1], 2);
-    // mitteln Punk
-    drawCircle_px(275, 270, 5, colorarray[1], 3);
+    drawCircle_px(275, 270, 235, colorarray[1], 4);
 
-    drawCircle_px(275, 270, 30, colorarray[6], 1);
+    // mitteln Punk
+    drawCircle_px(275, 270, 6, colorarray[1], 4);
+
+    // vong tron mau vang ben trong
+    drawCircle_px(275, 270, 30, colorarray[6], 2);
 
     // unter black line
     drawline_H(0, 550, 470, colorarray[0], 10);
-    drawline_H(154, 395, 470, colorarray[1], 2);
+    drawline_H(154, 395, 470, colorarray[1], 3);
+}
 
-    // gach vang va so
-    drawline_V(48, 65, 275, colorarray[3], 1);
-    print_string1216("120", 75, 257, colorarray[6], colorarray[0]);
-
-    drawline_H(53,70, 270, colorarray[3], 1);
-    print_string1216("30", 265, 78 , colorarray[6], colorarray[0]);
-
-    drawline_H(479,496, 270, colorarray[3], 1);
-    print_string1216("210", 265, 437 , colorarray[6], colorarray[0]);
-
-    // void write_line(short x1, short y1, short x2, short y2, int color)
-    drawline(88, 390,105, 382, colorarray[3]);
-    print_string1216("0", 368, 108, colorarray[6], colorarray[0]);
-
-
-    //number on display
-    print_string1216("Km/h", 380, 253, colorarray[1], colorarray[0]);
-
+void display_number(void)
+{
     print_string1216("000,00", 400, 245, colorarray[0], colorarray[6]);
-
-
 }
 
 void wait(int time){
@@ -91,37 +143,8 @@ void main(void)
 
     configPorts(); // Init Port N for User LED and Port P for digital Input from Motor
     init_and_paintbackground();
-    j = 0;
-
-    // Start endless loop
-    /*
-    while (1)
-    {
-        // Test Motor and Display
-        printf("Write rectangles\n"); // for debug only
-        for (x = 0; x <= (MAX_X) - 1; x = x + 30)
-            for (y = 0; y <= (MAX_Y) - 1; y = y + 30)
-            {
-                // Copy Digital Input PP0 (S1), PP1 (S2) to Port PN0 and PN1
-                GPIO_PORTN_DATA_R = GPIO_PORTP_DATA_R & 0x03;
-
-                window_set(x, y, x + 14, y + 14);
-                write_command(0x2C); //write pixel command
-                color = colorarray[(j) % 7];
-                j++; // change color
-
-                for (i = 0; i < (15 * 15); i++) // set pixels
-                {
-                    write_data((color >> 16) & 0xff); // red
-                    write_data((color >> 8) & 0xff); // green
-                    write_data((color) & 0xff); // blue
-                }
-            }
-    }
-    */
 
     drawline(275, 270 ,120, 350, colorarray[3]);
-    float f = 0;
 
     while (1)
        {
