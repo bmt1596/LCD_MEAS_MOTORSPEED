a257b8c6c2c99f187c021042f4b116f602dfd1ac
MINH TUNG BUI <minhtung.bui@recalm.com>
2020-12-12 21:46:35 +0100

    add: interrupt for LCD, Sensor

--- main.c
+++ main.c
@@ -5,23 +5,21 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "driverlib/systick.h"   //  Tivaware functions: SysTick...
+#include "driverlib/interrupt.h" //  Tivaware functions: Int...
+#include "driverlib/sysctl.h"    //  Tivaware functions: SysCtl...  + Macros  SYSCTL_...
+#include "driverlib/gpio.h"      //  Tivaware functions: GPIO... + Macros  GPIO_...
+#include "driverlib/timer.h"
+
 //void display_layout(void);
 //void display_project_information(void);
-void display_number_and_line(void);
+void Timer1_DisplayIntHandler(void);
 void wait(int time);
-//void edgeCountTimer(void);
-void tt_handler(void);
-void init_clock(void);
 
-uint32_t sysClock;
+uint32_t sysClock, timerScaler;
 uint32_t geschwindigkeit = 0;
-uint32_t global_count = 0;
 
-/*
- * Funktion zum display die Geschwindigkeit und die rote Line
-
- */
-void display_number_and_line(void)
+void Timer1_DisplayIntHandler(void)
 {
     static uint16_t radius = 160;
     static uint16_t phinull = 150;
@@ -29,6 +27,7 @@ void display_number_and_line(void)
     static uint16_t x, y;
     static uint16_t x_old = X_CENTER, y_old = Y_CENTER;
 
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
     char buffer[20];
     sprintf(buffer, "%3d km/h", geschwindigkeit);
     print_string1216(buffer, 400, 245, COLOR_BLACK, COLOR_YELLO);
@@ -62,58 +61,105 @@ void wait(int time)
     for (tmp = 0; tmp < 10800 * time; tmp++); // ~ 1ms
 }
 
+void stop_handler(void)
+{
+    GPIOIntClear(GPIO_PORTP_BASE,GPIO_PIN_0); // finially not needed, but done as a matter of principle
+    SysTickDisable(); // stop  systick
 
-// Function to calculate the kilometers of the day
-float clacKM(void){
-    // Wheels = 20'' = 50,8cm --> circumference = 159.593
-    float circumference = 159.593;
-    return circumference * global_count;
+    geschwindigkeit++;
+    if (geschwindigkeit >= 240)
+    {
+        geschwindigkeit = 0;
+    }
+    printf("%d\n",geschwindigkeit);
+
 }
 
-// Function to calculate the actual speed
-int calcSpeed(void){
-    // The x value should be the max cycle you can have so that ....
-    int x = 1;
-    return x/240;
+void test(void)
+{
+    TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
+    geschwindigkeit = 0;
 }
+
 void main(void)
 {
     int i = 0;
+    IntMasterDisable();        // as matter of principle
     // Set system frequency to 120 MHz
-    // Ist jetzt in timer.c
+    sysClock = SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 120000000);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);    // Clock Gate enable TIMER0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);    // Clock Gate enable TIMER1
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);    // Clock Gate enable TIMER2
+    SysCtlDelay(10);
+
     init_and_config_display();
     init_and_config_sensor();
-    init_clock();
-    tt_handler();
+    // dis play complett number 0-240 and circle
+    display_layout();
+    Timer1_DisplayIntHandler();
 
     // Test
     //timerLCD();
     // Test Ende
 
-    // dis play complett number 0-240 and circle
-    display_layout();
-
-    // test funtion to display the number and red line
+    /*
     for (i = 0; i <= 240; i++)
     {
         geschwindigkeit = i;
-        wait(10);
-        display_number_and_line();
+        //wait(10);
+        Timer1_DisplayIntHandler();
     }
 
     for (i = 240; i >= 0; i--)
     {
         geschwindigkeit = i;
-        wait(2);
-        display_number_and_line();
+        //wait(2);
+        Timer1_DisplayIntHandler();
     }
+    */
 
 
+    //periphery clock enable
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOP);
+    //pin setup
+    GPIOPinTypeGPIOInput(GPIO_PORTP_BASE, GPIO_PIN_0);
+    // Rising edge type interrupt
+    GPIOIntTypeSet(GPIO_PORTP_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
+    // "register" entry in  a copied IVT
+    GPIOIntRegister(GPIO_PORTP_BASE, stop_handler);
+    GPIOIntClear(GPIO_PORTP_BASE, GPIO_PIN_0); // optional ...
+    IntPrioritySet(INT_GPIOP0, 0x20); //high prio
+    GPIOIntEnable(GPIO_PORTP_BASE, GPIO_PIN_0); // Allow request output from Port unit
+    IntEnable(INT_GPIOP0);  // Allow request input to NVIC
+
+    // Configure Timer0 Interrupt
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC_UP);
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    // Configure Timer1 Interrupt
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER1_BASE, TIMER_A, sysClock / 20);      // fires every 50 ms
+    TimerIntRegister(TIMER1_BASE, TIMER_A, Timer1_DisplayIntHandler);
+    IntEnable(INT_TIMER1A);
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER1_BASE, TIMER_A);
+
+    // Configure Timer2 Interrupt
+    TimerConfigure(TIMER2_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER2_BASE, TIMER_A, sysClock / 2);      // fires every 500 ms
+    TimerIntRegister(TIMER2_BASE, TIMER_A, test);
+    //IntPrioritySet(INT_TIMER2A, 0x80);
+
+    IntEnable(INT_TIMER2A);
+    TimerIntEnable(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER2_BASE, TIMER_A);
+
+    IntMasterEnable();
     while (1)
        {
         //Test Motor and Display
         //printf("Write rectangles\n"); // for debug only
-        GPIO_PORTN_DATA_R = GPIO_PORTP_DATA_R & 0x03;
+        // GPIO_PORTN_DATA_R = GPIO_PORTP_DATA_R & 0x03;
 
        }
 }
